72d008fe6159428ca47c87d5cb89ffadf81a5fd0
Matti Ranta <matti@mdranta.net>
2019-02-20 00:54:18 -0500

    generate output

--- build.go
+++ build.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"bytes"
-	"github.com/pelletier/go-toml"
+	"os"
 	"fmt"
+	"bytes"
 	"html/template"
+	"path/filepath"
 	"io/ioutil"
-	"net/http"
+
+	"github.com/BurntSushi/toml"
 )
 
+type Imports struct {
+  Package []Package
+}
+
 type Package struct {
-	Repo     string
-	Path     string
-	Packages []string
-	Url      string
+	Repo     string `toml:"repo"`
+	Path     string `toml:"path"`
+	Packages []string `toml:"packages"`
+	Url      string `toml:"url"`
 }
 
 func (p Package) GetRoutes() (ret []string) {
@@ -29,7 +35,12 @@ func loadConfig() (ret []Package, err error) {
 	if err != nil {
 		return
 	}
-  toml.Unmarshal(content, &ret)
+	
+	var im Imports
+	if _, err = toml.Decode(string(content), &im); err != nil {
+		return
+	}
+	ret = im.Package
 	return
 }
 
@@ -44,5 +55,37 @@ func loadRoutes(packages []Package) (routes map[string]Package) {
 	return
 }
 
+
 func main() {
+	packages, err := loadConfig()
+	if err != nil {
+		fmt.Errorf("%s\n", err)
+		return
+	}
+	routes := loadRoutes(packages)
+	t, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		fmt.Errorf("%s\n", err)
+		return
+	}
+	for route, pkg := range routes {
+		// generate file and write out
+		pkg.Path = "src.techknowlogick.com"+pkg.Path
+		pkgDir := filepath.Join("public", route)
+		if err := os.MkdirAll(pkgDir, 0755); err != nil {
+			fmt.Errorf("%s\n", err)
+			return
+		}
+		var page bytes.Buffer
+		if err := t.Execute(&page, pkg); err != nil {
+			fmt.Errorf("%s\n", err)
+			return
+		}
+
+		pkgFile := filepath.Join(pkgDir, "index.html")
+		if err := ioutil.WriteFile(pkgFile, page.Bytes(), 0755); err != nil {
+			fmt.Errorf("%s\n", err)
+			return
+		}
+	}
 }
